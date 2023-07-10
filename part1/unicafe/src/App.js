2cfbf562003d4d1f46ac951fd275b8ac6c422830
westonludeke <westonludeke@gmail.com>
2023-07-10 16:01:35 -0500

    finished 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,18 +1,32 @@
-import { useState } from 'react'
-
-// -----
+import { useState } from 'react';
 
 const Button = (props) => {
   return (
     <button onClick={props.handleClick}>
       {props.text}
     </button>
-  )
+  );
 }
+
+const Statistics = (props) => {
+  console.log('statistics props: ', props);
+  const { good, neutral, bad, total, avg, positive } = props;
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <div>good: {good}</div>
+      <div>neutral: {neutral}</div>
+      <div>bad: {bad}</div>
+      <div>all: {total}</div>
+      <div>average: {avg}</div>
+      <div>positive: {positive}%</div>
+    </div>
+  );
+};
 
 const App = () => {
   const name = 'give feedback';
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -27,16 +41,16 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral'/>
       <Button handleClick={() => setBad(bad + 1)} text='bad'/>
 
-      <h2>statistics</h2>
-      <div>good: {good}</div>
-      <div>neutral: {neutral}</div>
-      <div>bad: {bad}</div>
-      <div>all: {total}</div>
-      <div>average: {avg}</div>
-      <div>positive: {positive}%</div>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        avg={avg}
+        positive={positive}
+      />
     </div>
-  )
+  );
 }
 
-// -----
-export default App
+export default App;
