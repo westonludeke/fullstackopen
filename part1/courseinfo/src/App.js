1513f37c71cecb398fd3d93fb48339cc6ab7c97e
westonludeke <westonludeke@gmail.com>
2023-07-10 09:40:03 -0500

    finished exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,5 @@
 const Header = (props) => {
-  console.log('Header props: ', props);
+  console.log('Header props: ', props.course);
 
   return (
     <div>
@@ -9,7 +9,7 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-  console.log('Content props: ', props);
+  console.log('Content props: ', props.parts);
 
   return (
     <div>
@@ -40,27 +40,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ]
+  const course = { 
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
