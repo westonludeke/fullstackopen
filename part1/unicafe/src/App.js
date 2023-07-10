db741268ef759da8f2ca95aac52e8ca45021c521
westonludeke <westonludeke@gmail.com>
2023-07-10 15:23:56 -0500

    finished 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,36 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+import { useState } from 'react'
 
+// -----
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const App = () => {
+  const name = 'give feedback';
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <Button handleClick={() => setGood(good + 1)} text='good'/>
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral'/>
+      <Button handleClick={() => setBad(bad + 1)} text='bad'/>
+
+      <h2>statistics</h2>
+      <div>good: {good}</div>
+      <div>neutral: {neutral}</div>
+      <div>bad: {bad}</div>
+    </div>
+  )
+}
+
+// -----
 export default App
