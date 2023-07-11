f074952bfef68d267452343482b559b3f5724dc5
westonludeke <westonludeke@gmail.com>
2023-07-11 13:12:48 -0500

    removed unneeded files

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Note = ({ note }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <li>{note.content}</li>
+  )
 }
 
-export default App;
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <Note key={note.id} note={note} />)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
