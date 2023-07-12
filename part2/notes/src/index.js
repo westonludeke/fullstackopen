2cdc606bb85e68829e1c23eebc7f556fff13ce94
westonludeke <westonludeke@gmail.com>
2023-07-12 18:02:46 -0500

    updated index.js of notes project

--- part2/notes/src/index.js
+++ part2/notes/src/index.js
@@ -1,6 +1,11 @@
 import ReactDOM from "react-dom/client"
-
 import App from "./App"
+import axios from 'axios'
+
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data;
+  console.log(notes);
+});
 
 const notes = [
   {
