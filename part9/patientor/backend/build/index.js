6450afe520ec5b6bf5044e5f62ca20cce5e798ba
westonludeke <westonludeke@gmail.com>
2023-11-22 14:08:35 -0600

    add cors to index.ts and create new build

--- part9/patientor/backend/build/index.js
+++ part9/patientor/backend/build/index.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 const PORT = 3001;
 app.get('/ping', (_req, res) => {
     console.log('someone pinged here');
--- part9/patientor/backend/index.ts
+++ part9/patientor/backend/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3001;
 
