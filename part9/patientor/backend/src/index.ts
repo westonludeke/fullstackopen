f455f5f8620560425d2828cf3661b58a792a4a5a
westonludeke <westonludeke@gmail.com>
2023-11-24 12:24:40 -0600

    update index.ts with new use route

--- part9/patientor/backend/src/index.ts
+++ part9/patientor/backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
+import diaryRouter from './routes/diaries';
 import cors from 'cors';
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -11,6 +11,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diaries', diaryRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
