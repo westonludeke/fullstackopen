2a130d2dd9f4b5d3e02f32e1dd6178bff662e111
westonludeke <westonludeke@gmail.com>
2023-11-24 14:36:47 -0600

    update diaryService

--- part9/patientor/backend/src/services/diaryService.ts
+++ part9/patientor/backend/src/services/diaryService.ts
@@ -1,16 +1,15 @@
-import diaryData from '../../data/entries.json';
+import diaryData from '../../data/entries.json'
+import { Weather, Visibility, DiaryEntry } from '../types'
 
-import { DiaryEntry } from '../types';
-
-const diaries: DiaryEntry[] = diaryData;
+const diaries: DiaryEntry[] = diaryData as DiaryEntry[];
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
-};
+}
 
 const addDiary = () => {
   return null;
-};
+}
 
 export default {
   getEntries,
